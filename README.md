0e245146ccfe7ca897cb0905a8e677c29d3baa37
Fabio Carvalho <fabio22191@hotmail.com>
2024-11-12 00:03:36 -0300

    First

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# docker-go
+# Enviroment Go with Docker
+
+Use the following command to run the script:
+<br>
+
+```bash
+docker run -it --name nginx -d -p 8000:80 --mount type=volume,source=data,target=/usr/share/nginx/html nginx bash
+```
+
+<br>
+
+And after that you will see bash terminal, run the following command:
+
+<br>
+
+```bash
+go run main.go
+```
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+    fmt.Println("This is a test.")
+    fmt.Println("This is a new line.")
+    fmt.Println("This is another new line.")
+    fmt.Println("This is a third new line.")
+    fmt.Println("This is a fourth new line.")
+    fmt.Println("This is a fifth new line.")
+    fmt.Println("This is a sixth new line.")
+    fmt.Println("This is a seventh new line.")
+    fmt.Println("This is an eighth new line.")
+    fmt.Println("This is a ninth new line.")
+    fmt.Println("This is a tenth new line.")
+    fmt.Println("This is a eleventh new line.")
+    fmt.Println("This is a twelfth new line.")
+}
